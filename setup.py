78976cef08bb7bbc06ba7b0f6e27e766861ea0ca
Hiroshi Toyama <toyama0919@gmail.com>
2017-08-25 09:27:03 +0900

    v0.4.3.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except(IOError, ImportError):
     long_description = open(os.path.join(here, 'README.md')).read()
 
-version = '0.4.2'
+version = '0.4.3'
 
 install_requires = [
     'tabulate',
