18ef38919642e218f7149194ef32529da741348f
Hiroshi Toyama <toyama0919@gmail.com>
2017-08-28 19:38:12 +0900

    fix logger format.

--- src/auroraex/logger.py
+++ src/auroraex/logger.py
@@ -2,7 +2,7 @@ import logging
 from logging import getLogger, StreamHandler, INFO, DEBUG
 
 def get_logger(debug):
-    log_fmt = '%(asctime)s- %(name)s - %(levelname)s - %(message)s'
+    log_fmt = '%(asctime)s %(levelname)s - %(message)s'
     logging.basicConfig(format=log_fmt)
 
     logger = getLogger(__name__)
